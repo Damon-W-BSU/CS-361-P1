617c9c797411050708d2c8af4cbc2a9a5459293c
Alex Ramirez <alexramirezroble@u.boisestate.edu>
2026-02-17 21:58:51 -0700

    Added getState and addTransition

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -2,8 +2,10 @@ package fa.dfa;
 
 import fa.State;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Map;
 
 public class DFA implements DFAInterface {
 
@@ -12,13 +14,14 @@ public class DFA implements DFAInterface {
     final private Set<Character> sigma;
     private State startState;
     final private Set<State> finalStates;
+    private Map<State, Map<Character, State>> transitions;
 
     public DFA() {
         states = new HashSet<State>();
         sigma = new HashSet<>();
         startState = null;
         finalStates = new HashSet<State>();
-
+        transitions = new HashMap<>();
     }
 
     @Override
@@ -32,7 +35,10 @@ public class DFA implements DFAInterface {
         }
 
         //Add New State
-        states.add(new DFAState(name));
+        State newState = new DFAState(name);
+        states.add(newState);
+        //Add Transition
+        transitions.put(newState, new HashMap<>());
         return true;
     }
 
@@ -85,7 +91,14 @@ public class DFA implements DFAInterface {
 
     @Override
     public State getState(String name) {
-        // TODO: implement
+
+        //Look For State
+        for (State s : states){
+            if (s.getName().equals(name)){
+                return s;
+            }
+        }
+
         return null;
     }
 
@@ -101,10 +114,22 @@ public class DFA implements DFAInterface {
         return false;
     }
 
+    //AI Used HERE
     @Override
     public boolean addTransition(String fromState, String toState, char onSymb) {
-        // TODO: implement
-        return false;
+
+        State from = getState(fromState);
+        State to = getState(toState);
+
+        if (from == null || to == null) return false;
+        if (!sigma.contains(onSymb)) return false;
+
+        Map<Character, State> stateTransitions = transitions.get(from);
+
+        if (stateTransitions.containsKey(onSymb)) return false;
+
+        stateTransitions.put(onSymb, to);
+        return true;
     }
 
     @Override
